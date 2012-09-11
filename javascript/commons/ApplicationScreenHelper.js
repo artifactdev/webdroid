e06207180f3ae5a19585744b2a7a6b23117b3827
Markus Jahn <markus-jahn@gmx.net>
2012-09-11 11:33:47 +0200

    Basic Javascript created

---
+++ javascript/commons/ApplicationScreenHelper.js
@@ -0,0 +1,49 @@
+﻿/**
+ * (c) 2012 Markus Jahn / Jeremias Arnstadt
+ */
+
+var ApplicationScreenHelper = {
+
+	applicationCounter : 0,
+
+	createNewApplication : function() {
+		var id = ApplicationScreenHelper.applicationCounter++;
+		var element = $( "<div />", {"id" : "deviceApplication_" + id , "class" : "deviceApplication"}).appendTo( deviceDisplay );
+		return { id : id, element : element };
+	},
+	
+	closeApplication : function( id ) {
+		var application = ApplicationScreenHelper.findApplication( id );
+		if (application != null) {
+			application.hide();
+		}
+	},
+	
+	showApplication : function( id ) {
+		var application = ApplicationScreenHelper.findApplication( id );
+		if (application == null) {
+			application = ApplicationScreenHelper.createNewApplication();
+		}
+		application.element.show();
+		return application;
+	},
+	
+	hideApplication : function( id ) {
+		var application = ApplicationScreenHelper.findApplication( id );
+		if (application != null) {
+			application.element.hide();
+		}
+	},
+	
+	findApplication : function( id ) {
+		if (id == undefined || id == null) {
+			return null;
+		}
+		
+		var applicationElement = $( "#deviceApplication_" + id );
+		if (applicationElement.length > 0) {
+			return { id : id, element : applicationElement };
+		}
+		return null;
+	}
+};
--- javascript/commons/GuiHandler.js
+++ javascript/commons/GuiHandler.js
@@ -1,10 +1,35 @@
 ﻿/**
  * (c) 2012 Markus Jahn / Jeremias Arnstadt
  */
-
+var deviceDisplay = null;
 var GuiHandler = {
 
-	init : function() {
-	}
+	idOfScreenSaver : null,
 
+	initApplication : function() {
+		deviceDisplay = $( "#deviceDisplay" );
+		
+		$( "#deviceOnOffSwitch" ).click(function() {
+			GuiHandler.showScreenSaver();
+			$( this ).unbind().click(function() {
+				GuiHandler.hideScreenSaver();
+			});
+		});
+		
+		//GuiHandler.showScreenSaver();
+		
+	},
+	
+	showScreenSaver : function() {
+		var application = ApplicationScreenHelper.showApplication( GuiHandler.idOfScreenSaver );
+		GuiHandler.idOfScreenSaver = GuiHandler.idOfScreenSaver == null ? application.id : null;
+	},
+
+	hideScreenSaver : function() {
+		ApplicationScreenHelper.hideApplication( GuiHandler.idOfScreenSaver );
+	},
+	
+	
 };
+
+$(GuiHandler.initApplication);
