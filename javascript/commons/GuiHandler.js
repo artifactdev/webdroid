79d98d01138b7dd26117ee5890f9c26a7d670374
Markus Jahn <markus-jahn@gmx.net>
2012-09-11 11:53:07 +0200

    BugFix

--- javascript/commons/GuiHandler.js
+++ javascript/commons/GuiHandler.js
@@ -2,33 +2,52 @@
  * (c) 2012 Markus Jahn / Jeremias Arnstadt
  */
 var deviceDisplay = null;
+var deviceInStandBy = false;
 var GuiHandler = {
 
-	idOfScreenSaver : null,
+	idOfLockScreen : null,
+	idOfStandByScreen : null,
 
 	initApplication : function() {
 		deviceDisplay = $( "#deviceDisplay" );
+		GuiHandler.initDeviceOnOffSwitch();
 		
-		$( "#deviceOnOffSwitch" ).click(function() {
-			GuiHandler.showScreenSaver();
-			$( this ).unbind().click(function() {
-				GuiHandler.hideScreenSaver();
-			});
-		});
 		
 		//GuiHandler.showScreenSaver();
 		
 	},
 	
-	showScreenSaver : function() {
-		var application = ApplicationScreenHelper.showApplication( GuiHandler.idOfScreenSaver );
-		GuiHandler.idOfScreenSaver = GuiHandler.idOfScreenSaver == null ? application.id : null;
+	showLockScreen : function() {
+		var application = ApplicationScreenHelper.showApplication( GuiHandler.idOfLockScreen );
+		GuiHandler.idOfLockScreen = GuiHandler.idOfLockScreen == null ? application.id : GuiHandler.idOfLockScreen;
 	},
 
-	hideScreenSaver : function() {
-		ApplicationScreenHelper.hideApplication( GuiHandler.idOfScreenSaver );
+	hideLockScreen : function() {
+		ApplicationScreenHelper.hideApplication( GuiHandler.idOfLockScreen );
 	},
 	
+	initDeviceOnOffSwitch : function() {
+		$( "#deviceOnOffSwitch" ).unbind().click(function() {
+			if (deviceInStandBy) {
+				GuiHandler.hideStandByScreen();
+				GuiHandler.showLockScreen();
+			} else {
+				GuiHandler.showStandByScreen();
+			}
+			deviceInStandBy = !deviceInStandBy;
+		});	
+	},
+	
+	showStandByScreen : function() {
+		GuiHandler.hideLockScreen();
+		var application = ApplicationScreenHelper.showApplication( GuiHandler.idOfStandByScreen );
+		GuiHandler.idOfStandByScreen = GuiHandler.idOfStandByScreen == null ? application.id : GuiHandler.idOfStandByScreen;
+		application.element.addClass( "standByScreen" );
+	},
+
+	hideStandByScreen : function() {
+		ApplicationScreenHelper.hideApplication( GuiHandler.idOfStandByScreen );
+	}
 	
 };
 
